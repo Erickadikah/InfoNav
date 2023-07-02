bcb4ce6750af50d201ec7219af5a8470f0f5e769
erickadikah <erickadikah2030@gmail.com>
2023-07-03 01:30:20 +0300

    ui landing page implemented

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -7,6 +7,9 @@ import { Link } from "react-router-dom";
 import { Info } from "@material-ui/icons";
 import Footer from "../components/Footer/index";
 import Services from "../components/Services/index";
+import icon from "../images/share.svg";
+import image from "../images/image2.svg";
+import share from "../images/ideas.svg";
 
 function LandingPage() {
   return (
@@ -45,7 +48,7 @@ function LandingPage() {
                   style={{
                     height: "50px",
                     width: "200px",
-                    borderRadius: "5px",
+                    borderRadius: "10px",
                     border: "1px solid white",
                   }}
                 >
@@ -63,7 +66,7 @@ function LandingPage() {
                     height: "50px",
                     width: "200px",
                     background: "#187741",
-                    borderRadius: "5px",
+                    borderRadius: "10px",
                     border: "1px solid white",
                   }}
                 >
@@ -81,39 +84,97 @@ function LandingPage() {
           </article>
         </div>
       </div>
-  <div className="container-2" style={{ width: "50%", marginRight: '20px'}}>
-  <div className="firstContent">
-    <article>
-      <h1 style={{ fontWeight: "bold", justifyContent: "center" }}>
-        No more Searching for answer on google! Get your answers from the
-        best
-        <span style={{ color: "#187741" }}> Agriculture Solutions </span>
-        <br></br>
-        across all your
-        <span style={{ color: "#187741" }}> devices with Info nav</span>
-      </h1>
-      <p
-        style={{
-          fontFamily: "sans-serif",
-          lineHeight: "1.5",
-        }}
+      <div
+        className="container-2"
+        style={{ width: "50%", marginRight: "20px" }}
       >
-        InfoNav is a platform that help you get your information powored
-        by AI chat with our model today for free. Our AI-enhanced website
-        leverages advanced machine learning algorithms to provide farmers
-        with real-time weather forecasts, market prices, and disease
-        control methods. This information is delivered through an
-        easy-to-use interface, making it accessible to even the most
-        technologically challenged farmers. We are proud to be using our
-        skills and expertise to make a positive impact on the lives of
-        farmers in Africa. time.
-      </p>
-    </article>
-  </div>
-  <div>
-  <h1>Right side</h1>
-  </div>
-</div>
+        <div className="firstContent">
+          <article style={{ marginLeft: "50px", marginBottom: "50px" }}>
+            <h1 style={{ fontWeight: "bold" }}>
+              No more Searching for answer on google! Get your answers from the
+              best
+              <span style={{ color: "#187741" }}> Agriculture Solutions </span>
+              <br></br>
+              across all your
+              <span style={{ color: "#187741" }}> devices with Info nav</span>
+            </h1>
+            <p
+              style={{
+                fontFamily: "sans-serif",
+                lineHeight: "1.5",
+              }}
+            >
+              InfoNav is a platform that help you get your information powored
+              by AI chat with our model today for free. Our AI-enhanced website
+              leverages advanced machine learning algorithms to provide farmers
+              with real-time weather forecasts, market prices, and disease
+              control methods.
+            </p>
+          </article>
+          <div className="right_Side" style={{ marginLeft: "200px" }}>
+            <h1>Get it now with InfoNav</h1>
+            <div
+              style={{ display: "flex", gap: "100px", alignItems: "center" }}
+            >
+              <div>
+                <img
+                  src={icon}
+                  alt="icon"
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    marginBottom: "10px",
+                  }}
+                />
+                <p style={{ textAlign: "center" }}>Great growth</p>
+              </div>
+              <div>
+                <img
+                  src={image}
+                  alt="image"
+                  style={{
+                    width: "150%",
+                    height: "150%",
+                    marginBottom: "10px",
+                  }}
+                />
+                <p style={{ textAlign: "center" }}>Share Ideas</p>
+              </div>
+              <div>
+                <img
+                  src={share}
+                  alt="image"
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    marginBottom: "10px",
+                  }}
+                />
+                <p style={{ textAlign: "center" }}>Link To Up</p>
+              </div>
+            </div>
+            <div style={{ marginTop: '50px'}}>
+            <span
+              className="top_button"
+              style={{
+                height: "50px",
+                width: "200px",
+                borderRadius: "5px",
+                border: "1px solid white",
+              }}
+            >
+              <Link
+                className="text_decorator"
+                to="/signup"
+                style={{ color: "white" }}
+              >
+                Get started Now !
+              </Link>
+            </span>
+            </div>
+          </div>
+        </div>
+              </div>
       <Services />
       <div>
         <Footer />
--- client/src/components/common/header/Header.jsx
+++ client/src/components/common/header/Header.jsx
@@ -1,32 +1,46 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
-function Header () {
+function Header() {
   return (
     <section className="header border">
       <section className="header-top">
-        <section className="header-top__logo">      
-        <img src="./mylogo.svg" alt="ai" style={{
-        width: '100%',
-        height: '65%',
-        }}/>
-          <a href="/" className="header-logo" style={{
-            fontSize: '1rem',
-            color: 'white'
-         }}>Info Nav</a>
+        <section className="header-top__logo">
+          <img
+            src="./mylogo.svg"
+            alt="ai"
+            style={{
+              width: "100%",
+              height: "65%",
+            }}
+          />
+          <a
+            href="/"
+            className="header-logo"
+            style={{
+              fontSize: "1rem",
+              color: "white",
+            }}
+          >
+            Info Nav
+          </a>
         </section>
         <section className="header-top__navbar">
-          <hr className="header-top__seperator" />
-        </section>
-      </section>
-      <section className="header-bottom">
-        <section className="header-bottom__phone">     
-        </section>
-        <section className="header-bottom__email">
+            <Button variant="outlined" color="primary">
+              Signup
+            </Button>
+          <a to="/signin">
+          <Button variant="outlined" color="primary">
+            {" "}
+            Login
+          </Button>
+          </a>
         </section>
       </section>
     </section>
-  )
+  );
 }
 
 export default Header;
